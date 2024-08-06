5b3302923f9f15f91d364b632e7ad81bff469b8c
the-darklord <saimihirnath@protonmail.com>
2024-08-06 19:33:24 +0530

    Updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
 	try {
+		mongoose.set('strictQuery', false);
 		await mongoose.connect(process.env.MONGODB_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		});
 		console.log("MongoDB Connected");
 	} catch (error) {
-		console.error("Error connecting to MongoDB:", error.message);
+		console.error("Error connecting to MongoDB:", error);
 		process.exit(1);
 	}
 };
